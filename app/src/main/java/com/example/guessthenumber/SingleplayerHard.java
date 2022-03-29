f378cfdd9537a30605d8dab15bd92cc9c74e0768
Mikhail2Git <mm22@my.yorku.ca>
2022-03-29 13:06:25 -0400

    Added normal and hard singleplayer mode.

--- app/src/main/java/com/example/guessthenumber/SingleplayerHard.java
+++ app/src/main/java/com/example/guessthenumber/SingleplayerHard.java
@@ -1,26 +1,25 @@
 package com.example.guessthenumber;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class SingleplayerEasy extends AppCompatActivity {
-
+public class SingleplayerHard extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_singleplayer_easy);
+        setContentView(R.layout.activity_singleplayer_hard);
     }
-    int randomNum = (int) Math.floor(Math.random()*(10)+1);
+    int randomNum = (int) Math.floor(Math.random()*(100)+1);
 
     public void buttonClicked(View v) {
         EditText inputBox = (EditText) findViewById(R.id.input);
         String str = inputBox.getText().toString();
 
-        int guess = Integer.parseInt(str);;
+        int guess = Integer.parseInt(str);
         String isSame = guess+" "+randomNum+" "+"wrong";
         if (guess == randomNum)
             isSame = guess+" "+randomNum+" "+"right";;
